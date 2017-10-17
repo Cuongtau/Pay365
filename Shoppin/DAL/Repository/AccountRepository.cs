bbe442809d27d386ddc4d76a0387d00ce6c3c921
THANGNN <THANGNN@DESKTOP-FPJGDPJ>
2017-10-17 10:25:40 +0700

    len 4

--- Shoppin/DAL/Repository/AccountRepository.cs
+++ Shoppin/DAL/Repository/AccountRepository.cs
@@ -23,8 +23,6 @@ namespace DAL.Repository
         /// <returns></returns>
         public int Register(Account account)
         {
-            try
-            {
                 var pars = new SqlParameter[5];
                 pars[0] = new SqlParameter("@_AccountName", account.AccountName);
                 pars[1] = new SqlParameter("@_Password", account.Password);
@@ -32,13 +30,7 @@ namespace DAL.Repository
                 pars[3] = new SqlParameter("@_PhoneNumber", account.PhoneNumber);
                 pars[4] = new SqlParameter("@_ResponseCode", SqlDbType.Int) { Direction = ParameterDirection.Output };
                 new DBHelper(connectString).ExecuteNonQuerySP("SP_RegisterAccount", pars);
-                return Convert.ToInt32(pars[4].Value);
-            }
-            catch (Exception ex)
-            {
-                NLogLogger.PublishException(ex);
-                return -969;
-            }
+                return Convert.ToInt32(pars[4].Value);           
         }
     }
 }
